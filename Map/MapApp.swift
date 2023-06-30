e213a074af32186d60cd43fbb7fd989f08955078
Yaroslav Zagumennikov <91648112+Yarik-Ydavik@users.noreply.github.com>
2023-06-30 23:55:30 +0500

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Yaroslav Zagumennikov on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
